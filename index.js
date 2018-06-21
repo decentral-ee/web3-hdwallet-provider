250562b962c8bdf7d4a1d08f1c9077bfd5cda210
Martynas Žilinskas <martynas@quatrodev.com>
2018-06-21 21:56:25 +0300

    v0.0.4

--- index.js
+++ index.js
@@ -1,94 +1,107 @@
-const debug = require('debug');
+const debug = require("debug");
 const logger = {
-    info : debug('hdwallet:info'),
-    warn : debug('hdwallet:warn'),
-    error : debug('hdwallet:error')   
+    info: debug("hdwallet:info"),
+    warn: debug("hdwallet:warn"),
+    error: debug("hdwallet:error")
 };
 var bip39 = require("bip39");
-var hdkey = require('ethereumjs-wallet/hdkey');
+var hdkey = require("ethereumjs-wallet/hdkey");
 var ProviderEngine = require("web3-provider-engine");
-var FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
-var HookedSubprovider = require('web3-provider-engine/subproviders/hooked-wallet.js');
+var FiltersSubprovider = require("web3-provider-engine/subproviders/filters.js");
+var HookedSubprovider = require("web3-provider-engine/subproviders/hooked-wallet.js");
 var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
 var Web3 = require("web3");
-var Transaction = require('ethereumjs-tx');
-var ethUtil = require('ethereumjs-util');
+var Transaction = require("ethereumjs-tx");
+var ethUtil = require("ethereumjs-util");
 
-function HDWalletProvider(mnemonic, provider_url, address_index=0, num_addresses=1) {
-  this.mnemonic = mnemonic;
-  this.hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
-  this.wallet_hdpath = "m/44'/60'/0'/0/";
-  this.wallets = {};
-  this.addresses = [];
+function HDWalletProvider(mnemonic, provider_url, address_index = 0, num_addresses = 1) {
+    this.mnemonic = mnemonic;
+    this.hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
+    this.wallet_hdpath = "m/44'/60'/0'/0/";
+    this.wallets = {};
+    this.addresses = [];
 
-  for (let i = address_index; i < address_index + num_addresses; i++){
-    var wallet = this.hdwallet.derivePath(this.wallet_hdpath + i).getWallet();
-    var addr = '0x' + wallet.getAddress().toString('hex');
-    this.addresses.push(addr);
-    this.wallets[addr] = wallet;
-  }
+    for (let i = address_index; i < address_index + num_addresses; i++) {
+        var wallet = this.hdwallet.derivePath(this.wallet_hdpath + i).getWallet();
+        var addr = "0x" + wallet.getAddress().toString("hex");
+        this.addresses.push(addr);
+        this.wallets[addr] = wallet;
+    }
 
-  const tmp_accounts = this.addresses;
-  const tmp_wallets = this.wallets;
+    const tmp_accounts = this.addresses;
+    const tmp_wallets = this.wallets;
 
-  this.engine = new ProviderEngine();
-  this.engine.addProvider(new HookedSubprovider({
-    getAccounts: function(cb) { cb(null, tmp_accounts) },
-    getPrivateKey: function(address, cb) {
-      if (!tmp_wallets[address]) { return cb('Account not found'); }
-      else { cb(null, tmp_wallets[address].getPrivateKey().toString('hex')); }
-    },
-    signTransaction: function(txParams, cb) {
-      let pkey;
-      if (tmp_wallets[txParams.from]) { pkey = tmp_wallets[txParams.from].getPrivateKey(); }
-      else { cb('Account not found'); }
-      var tx = new Transaction(txParams);
-      tx.sign(pkey);
-      var rawTx = '0x' + tx.serialize().toString('hex');
-      cb(null, rawTx);
-    },
-    signMessage(message, cb) {
-      const dataIfExists = message.data;
-      if (!dataIfExists) {
-        cb('No data to sign');
-      }
-      if (!tmp_wallets[message.from]) {
-        cb('Account not found');
-      }
-      let pkey = tmp_wallets[message.from].getPrivateKey();
-      var dataBuff = ethUtil.toBuffer(dataIfExists);
-      var msgHashBuff = ethUtil.hashPersonalMessage(dataBuff);
-      var sig = ethUtil.ecsign(msgHashBuff, pkey);
-      var rpcSig = ethUtil.toRpcSig(sig.v, sig.r, sig.s);
-      cb(null, rpcSig);
-	},
-  }));
-  this.engine.addProvider(new FiltersSubprovider());
-  this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));
-  this.engine.start(); // Required by the provider engine.
-  this.engine.on('error', function (err) {
-    logger.error('web3 provider engine error', err);
-  });
-};
+    this.engine = new ProviderEngine();
+    this.engine.addProvider(
+        new HookedSubprovider({
+            getAccounts: function(cb) {
+                cb(null, tmp_accounts);
+            },
+            getPrivateKey: function(address, cb) {
+                if (!tmp_wallets[address]) {
+                    return cb("Account not found");
+                } else {
+                    cb(null, tmp_wallets[address].getPrivateKey().toString("hex"));
+                }
+            },
+            signTransaction: function(txParams, cb) {
+                let pkey;
+                if (tmp_wallets[txParams.from]) {
+                    pkey = tmp_wallets[txParams.from].getPrivateKey();
+                } else {
+                    cb("Account not found");
+                }
+                var tx = new Transaction(txParams);
+                tx.sign(pkey);
+                var rawTx = "0x" + tx.serialize().toString("hex");
+                cb(null, rawTx);
+            },
+            signMessage(message, cb) {
+                const dataIfExists = message.data;
+                if (!dataIfExists) {
+                    cb("No data to sign");
+                }
+                if (!tmp_wallets[message.from]) {
+                    cb("Account not found");
+                }
+                let pkey = tmp_wallets[message.from].getPrivateKey();
+                var dataBuff = ethUtil.toBuffer(dataIfExists);
+                var msgHashBuff = ethUtil.hashPersonalMessage(dataBuff);
+                var sig = ethUtil.ecsign(msgHashBuff, pkey);
+                var rpcSig = ethUtil.toRpcSig(sig.v, sig.r, sig.s);
+                cb(null, rpcSig);
+            }
+        })
+    );
+    this.engine.addProvider(new FiltersSubprovider());
+    this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));
+    this.engine.start(); // Required by the provider engine.
+    this.engine.on("error", function(err) {
+        logger.error("web3 provider engine error", err);
+    });
+}
 
 HDWalletProvider.prototype.sendAsync = function() {
-  this.engine.sendAsync.apply(this.engine, arguments);
+    this.engine.sendAsync.apply(this.engine, arguments);
 };
 
 HDWalletProvider.prototype.send = function() {
-  return this.engine.send.apply(this.engine, arguments);
+    return this.engine.send.apply(this.engine, arguments);
 };
 
 // returns the address of the given address_index, first checking the cache
 HDWalletProvider.prototype.getAddress = function(idx) {
-  logger.info('getting addresses', this.addresses[0], idx)
-  if (!idx) { return this.addresses[0]; }
-  else { return this.addresses[idx]; }
-}
+    logger.info("getting addresses", this.addresses[0], idx);
+    if (!idx) {
+        return this.addresses[0];
+    } else {
+        return this.addresses[idx];
+    }
+};
 
 // returns the addresses cache
 HDWalletProvider.prototype.getAddresses = function() {
-  return this.addresses;
-}
+    return this.addresses;
+};
 
 module.exports = HDWalletProvider;
